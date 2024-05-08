a4f5cc55dcd5c9392e1feed87be2e918db8e8421
Peatruck <patrickmassonward@gmail.com>
2024-05-08 15:03:52 +0100

    Adding initial project files

---
+++ src/main/java/com/sparta/sqlworlddatabase/SqlWorldDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sqlworlddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlWorldDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlWorldDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/sqlworlddatabase/SqlWorldDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sqlworlddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlWorldDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
